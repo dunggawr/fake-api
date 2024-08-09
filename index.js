e59635828d0b9c4f18e3201cf21fb0ee693f883f
hotsooc <hotsooc@gmail.com>
2024-08-09 15:17:48 +0700

    update

--- index.js
+++ index.js
@@ -274,19 +274,19 @@ const fakeData = {
         writing: [
             {
                 title: "Bài 6. Write Task 2: Các loại trong một nốt nhạc",
-                image: "/images/library/74bbca6266c0e208f4d14b8c2e35a53a",
+                image: "/images/library/74bbca6266c0e208f4d14b8c2e35a53a.png",
                 view: "102 lượt xem",
                 time: "2 năm trước"
             },
             {
                 title: "Bài 5. Write Task1 + 2: Học ở bậc gì, cũng KHÔNG thoát các ...",
-                image: "/images/library/529fbadf9a788da26f816e8f785fa0c1",
+                image: "/images/library/529fbadf9a788da26f816e8f785fa0c1.png",
                 view: "102 lượt xem",
                 time: "2 năm trước"
             },
             {
                 title: "Bài 6. Write Task 2: Các loại trong một nốt nhạc",
-                image: "/images/library/74bbca6266c0e208f4d14b8c2e35a53a",
+                image: "/images/library/74bbca6266c0e208f4d14b8c2e35a53a.png",
                 view: "102 lượt xem",
                 time: "2 năm trước"
             },
